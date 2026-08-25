9c465371d140992c8d38977b3fcee6f76f6ced63
tes12886-sudo <tes12886@gmail.com>
2026-08-25 12:58:27 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,8 +22,34 @@ MAIN_KEY = b"Yg&tc%DEuh6%Zc^8"
 MAIN_IV = b"6oyZDr22E3ychjM%"
 BASE_TARGET_URL = "https://loginbp.ggpolarbear.com"
 
-# Data versi default
-VER_DATA = {
+# Konfigurasi Gamevar Custom (Termasuk float 8.5)
+CUSTOM_GAMEVAR = (
+    "var_name,comment,var_type,var_value,var_region,var_platform\n"
+    "var_name,comment,var_type,var_value,var_region,var_platform\n"
+    "EnableVariableFFVoiceIDC,EnableVariableFFVoiceIDC,bool,False,,\n"
+    "EnableYieldMutexDuringAsyncLoad,EnableYieldMutexDuringAsyncLoad,bool,False,,\n"
+    "NinthProgressLoadingDuration,NinthProgressLoadingDuration,float,0,,\n"
+    "EnableUGCScrollViewCulling,EnableUGCScrollViewCulling,bool,False,,\n"
+    "EnableUGCScrollViewCulling,EnableUGCScrollViewCulling,bool,False,,\n"
+    "ReservedInt01,ReservedInt01,int,5,,\n"
+    "NinthLevelPortalRadius,NinthLevelPortalRadius,float,20,,\n"
+    "Enable2018ABstreamed,Enable2018ABstreamed,bool,False,,ios\n"
+    "EnableAsyncCullResultsRelease,EnableAsyncCullResultsRelease,bool,False,,ios\n"
+    "ReservedInt02,ReservedInt02,int,30,,\n"
+    "EnableUGCHalfwayJoin,EnableUGCHalfwayJoin,bool,False,,\n"
+    "LadderMatchSplashRegionOn,LadderMatchSplashRegionOn,string,PK;EUROPE;TH;SG;TW;BR,,\n"
+    "SensitivityMaxSetting,SensitivityMaxSetting,float,8.5,,\n"
+    "Sensitivity1PMaxSetting,Sensitivity1PMaxSetting,float,8.5,,\n"
+    "X1ScopeMaxSetting,X1ScopeMaxSetting,float,8.5,,\n"
+    "X2ScopeMaxSetting,X2ScopeMaxSetting,float,8.5,,\n"
+    "X4ScopeMaxSetting,X4ScopeMaxSetting,float,8.5,,\n"
+    "X8ScopeMaxSetting,X8ScopeMaxSetting,float,8.5,,\n"
+    "FreeLookMaxSetting,FreeLookMaxSetting,float,8.5,,\n"
+    "PlayerOutlineWidthSpecial,PlayerOutlineWidthSpecial,float,8.5,,\n"
+)
+
+# Template Data Versi Free Fire Biasa (Prefix 1.x)
+VER_DATA_BIASA = {
     "code": 0,
     "is_server_open": True,
     "is_firewall_open": False,
@@ -69,9 +95,62 @@ VER_DATA = {
     "garena_login": False,
     "garena_hint": False,
     "gop_url": "",
-    "gamevar": "var_name,comment,var_type,var_value,var_region,var_platform\nvar_name,comment,var_type,var_value,var_region,var_platform\nEnableVariableFFVoiceIDC,EnableVariableFFVoiceIDC,bool,False,,\nEnableYieldMutexDuringAsyncLoad,EnableYieldMutexDuringAsyncLoad,bool,False,,\nNinthProgressLoadingDuration,NinthProgressLoadingDuration,float,0,,\nEnableUGCScrollViewCulling,EnableUGCScrollViewCulling,bool,False,,\nEnableUGCScrollViewCulling,EnableUGCScrollViewCulling,bool,False,,\nReservedInt01,ReservedInt01,int,5,,\nNinthLevelPortalRadius,NinthLevelPortalRadius,float,20,,\nEnable2018ABstreamed,Enable2018ABstreamed,bool,False,,ios\nEnableAsyncCullResultsRelease,EnableAsyncCullResultsRelease,bool,False,,ios\nReservedInt02,ReservedInt02,int,30,,\nEnableUGCHalfwayJoin,EnableUGCHalfwayJoin,bool,False,,\nLadderMatchSplashRegionOn,LadderMatchSplashRegionOn,string,PK;EUROPE;TH;SG;TW;BR,,\nSensitivityMaxSetting,SensitivityMaxSetting,float,8.5,,\nSensitivity1PMaxSetting,Sensitivity1PMaxSetting,float,8.5,,\nX1ScopeMaxSetting,X1ScopeMaxSetting,float,8.5,,\nX2ScopeMaxSetting,X2ScopeMaxSetting,float,8.5,,\nX4ScopeMaxSetting,X4ScopeMaxSetting,float,8.5,,\nX8ScopeMaxSetting,X8ScopeMaxSetting,float,8.5,,\nFreeLookMaxSetting,FreeLookMaxSetting,float,8.5,,\nPlayerOutlineWidthSpecial,PlayerOutlineWidthSpecial,float,8.5,,\n",
+    "gamevar": CUSTOM_GAMEVAR,
     "remote_option_version": "optionallocres:50|optionalavatarres:791|optionalclothres:1228|optionalfootballres:27|optionalfullscreencgres:319|optionalhuntinggroundres:246|optionalinfection:125|optionalingameres:503|optionallobbyres:640|optionallonewolfres:86|optionallonewolfstrikeoutres:59|optionalludores:42|optionalmap1res:385|optionalmap2res:156|optionalmap4res:139|optionalmaphippores:118|optionalmapres:357|optionalnewblast:163|optionalpetres:910|optionalrushb:108|optionalrushingpetsres:84|optionalsnowduelres:65|optionalsocialres:223|optionaltrainingres:297|optionalugcres:844|optionalvoiceres:344|optionalwerewolves:153|optionalwerunres:92|optionalmapponyres:204|optionalugcoldparadiseres:34|optionalmultiregionres:29",
     "remote_option_version_astc": "optionallocres:50|optionalavatarres:753|optionalclothres:1228|optionalfootballres:29|optionalfullscreencgres:306|optionalhuntinggroundres:216|optionalinfection:124|optionalingameres:461|optionallobbyres:640|optionallonewolfres:206|optionallonewolfstrikeoutres:155|optionalludores:175|optionalmap1res:385|optionalmap2res:192|optionalmap4res:175|optionalmaphippores:120|optionalmapres:391|optionalnewblast:162|optionalpetres:910|optionalrushb:241|optionalrushingpetsres:217|optionalsnowduelres:65|optionalsocialres:215|optionaltrainingres:267|optionalugcres:786|optionalvoiceres:379|optionalwerewolves:286|optionalwerunres:81|optionalmapponyres:204|optionalugcoldparadiseres:33|optionalmultiregionres:27",
+    "ggp_url": "gin.freefiremobile.com",
+}
+
+# Template Data Versi Free Fire MAX (Prefix 2.x)
+VER_DATA_MAX = {
+    "code": 0,
+    "is_server_open": True,
+    "is_firewall_open": False,
+    "cdn_url": "https://dl.gmc.freefiremobile.com/live/ABHotUpdates/",
+    "backup_cdn_url": "https://dl.gmc.freefiremobile.com/live/ABHotUpdates/",
+    "abhotupdate_cdn_url": "https://core-gmc.freefiremobile.com/live/ABHotUpdates/",
+    "img_cdn_url": "https://dl.gmc.freefiremobile.com/common/",
+    "login_download_optionalpack": "optionalclothres:shaders|optionalpetres:optionalpetres_commonab_shader|optionallobbyres:",
+    "need_track_hotupdate": True,
+    "abhotupdate_check": "cache_res;assetindexer;SH-Gpp",
+    "latest_release_version": "OB54",
+    "min_hint_size": 1,
+    "space_required_in_GB": 1.48,
+    "should_check_ab_load": False,
+    "force_refresh_restype": "optionalavatarres",
+    "remote_version": "2.130.22",
+    "server_url": "https://bahlil.embege-enak-loh.my.id/",
+    "is_review_server": False,
+    "use_login_optional_download": True,
+    "use_background_download": True,
+    "use_background_download_lobby": True,
+    "country_code": "SG",
+    "client_ip": "23.236.119.226",
+    "gdpr_version": 0,
+    "billboard_cdn_url": "https://dl.dir.freefiremobile.com/common/OB54/CSH/patchupdate/sgolzjifnmi101.ff_extend;https://dl.dir.freefiremobile.com/common/OB54/CSH/patchupdate/sgolzjifnmi102.ff_extend;https://dl.dir.freefiremobile.com/common/OB54/CSH/patchupdate/sgolzjifnmi103.ff_extend;https://dl.dir.freefiremobile.com/common/OB54/CSH/patchupdate/sgolzjifnmi104.ff_extend;https://dl.dir.freefiremobile.com/common/OB54/CSH/patchupdate/sgolzjifnmi105.ff_extend;https://dl.dir.freefiremobile.com/common/OB54/CSH/patchupdate/sgolzjifnmi106.ff_extend;https://dl.dir.freefiremobile.com/common/OB54/CSH/patchupdate/sgolzjifnmi107.ff_extend",
+    "billboard_msg": "",
+    "web_url": "",
+    "billboard_bg_url": "https://dl.cdn.freefiremobile.com/common/OB23/version/Patch_Bg.png",
+    "max_store": "",
+    "max_web": "",
+    "max_video": "",
+    "patchnote_url": "https://dl.dir.freefiremobile.com/common/web_event/aswqooiwd/zClWsKYO.html?lang=en",
+    "multi_region": "",
+    "need_check_ip_list": ["202.81.108.9"],
+    "network_log_server": "https://sgnetwork.ggblueshark.com/",
+    "web_log_server": "https://networkselftest.ff.garena.com/api/",
+    "login_failed_count": 2,
+    "test_url": "",
+    "core_url": "csoversea.castle.freefiremobile.com",
+    "core_ip_list": ["0.0.0.0", "50.109.27.134", "129.226.2.163", "129.226.1.13", "129.226.1.16"],
+    "appstore_url": "http://play.google.com/store/apps/details?id=com.dts.freefiremax",
+    "backup_appstore_url": "http://play.google.com/store/apps/details?id=com.dts.freefireth",
+    "garena_login": False,
+    "garena_hint": False,
+    "gop_url": "",
+    "gamevar": CUSTOM_GAMEVAR,
+    "remote_option_version": "optionallocres:50|optionalavatarres:708|optionalclothres:1150|optionalfootballres:47|optionalfullscreencgres:334|optionalhuntinggroundres:178|optionalinfection:121|optionalingameres:469|optionallobbyres:610|optionallonewolfres:77|optionallonewolfstrikeoutres:23|optionalludores:40|optionalmap1res:385|optionalmap2res:125|optionalmap4res:110|optionalmaphippores:90|optionalmapres:340|optionalnewblast:138|optionalpetres:848|optionalrushb:123|optionalrushingpetsres:88|optionalsnowduelres:59|optionaltrainingres:83|optionalugcres:537|optionalvoiceres:355|optionalwerewolves:173|optionalmapponyres:200|optionalsocialres:111|optionalwerunres:83|optionalugcoldparadiseres:32|optionalmultiregionres:25",
+    "remote_option_version_astc": "optionallocres:50|optionalavatarres:711|optionalclothres:1150|optionalfootballres:38|optionalfullscreencgres:318|optionalhuntinggroundres:178|optionalinfection:116|optionalingameres:438|optionallobbyres:593|optionallonewolfres:139|optionallonewolfstrikeoutres:96|optionalludores:144|optionalmap1res:385|optionalmap2res:159|optionalmap4res:144|optionalmaphippores:92|optionalmapres:374|optionalnewblast:138|optionalpetres:848|optionalrushb:227|optionalrushingpetsres:192|optionalsnowduelres:59|optionaltrainingres:79|optionalugcres:507|optionalvoiceres:388|optionalwerewolves:277|optionalmapponyres:200|optionalsocialres:106|optionalwerunres:74|optionalugcoldparadiseres:32|optionalmultiregionres:26",
     "ggp_url": "gin.freefiremobile.com",
 }
 
@@ -214,42 +293,17 @@ async def handle_major_login(request: Request):
 async def catch_all(request: Request, path: str):
     # Cek apakah path mengandung 'ver.php'
     if "ver.php" in path.lower() or path.lower().endswith("ver.php"):
-        response_data = VER_DATA.copy()
-        
-        client_version = request.query_params.get("version")
-        
-        if client_version:
-            major_prefix = client_version.split(".")[0]
-            response_data["remote_version"] = f"{major_prefix}.130.22"
-            
-            # Konfigurasi khusus Free Fire MAX (Prefix 2.x.x)
-            if major_prefix == "2":
-                response_data["cdn_url"] = "https://dl.gmc.freefiremobile.com/live/ABHotUpdates/"
-                response_data["backup_cdn_url"] = "https://dl.gmc.freefiremobile.com/live/ABHotUpdates/"
-                response_data["abhotupdate_cdn_url"] = "https://core-gmc.freefiremobile.com/live/ABHotUpdates/"
-                response_data["use_background_download"] = True
-                response_data["use_background_download_lobby"] = True
+        client_version = request.query_params.get("version", "1.130.22")
+        major_prefix = client_version.split(".")[0]
 
-                opt_ver = response_data["remote_option_version"]
-                opt_ver = re.sub(r"optionalavatarres:\d+", "optionalavatarres:708", opt_ver)
-                opt_ver = re.sub(r"optionalclothres:\d+", "optionalclothres:1150", opt_ver)
-                opt_ver = re.sub(r"optionalpetres:\d+", "optionalpetres:848", opt_ver)
-                response_data["remote_option_version"] = opt_ver
-            
-            # Konfigurasi khusus Free Fire Biasa (Prefix 1.x.x)
-            elif major_prefix == "1":
-                response_data["cdn_url"] = "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/"
-                response_data["backup_cdn_url"] = "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/"
-                response_data["abhotupdate_cdn_url"] = "https://dl-core.cdn.freefiremobile.com/live/ABHotUpdates/"
-                response_data["use_background_download"] = False
-                response_data["use_background_download_lobby"] = False
+        # Pilih struktur data template berdasarkan prefix versi
+        if major_prefix == "2":
+            response_data = VER_DATA_MAX.copy()
+            response_data["remote_version"] = f"2.130.22"
+        else:
+            response_data = VER_DATA_BIASA.copy()
+            response_data["remote_version"] = f"1.130.22"
 
-                opt_ver = response_data["remote_option_version"]
-                opt_ver = re.sub(r"optionalavatarres:\d+", "optionalavatarres:791", opt_ver)
-                opt_ver = re.sub(r"optionalclothres:\d+", "optionalclothres:1228", opt_ver)
-                opt_ver = re.sub(r"optionalpetres:\d+", "optionalpetres:910", opt_ver)
-                response_data["remote_option_version"] = opt_ver
-            
         return JSONResponse(content=response_data, status_code=200)
 
     # Forward semua path lainnya ke target URL
